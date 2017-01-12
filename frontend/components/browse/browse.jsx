969aad366adbb28fad78bda884be3ce61b0fa95d
Cody <codhah92@gmail.com>
2017-01-12 14:13:02 -0500

    Edit browse page

--- frontend/components/browse/browse.jsx
+++ frontend/components/browse/browse.jsx
@@ -3,15 +3,39 @@ import { Link, withRouter } from 'react-router';
 import DashboardContainer from '../dashboard/dashboard_container';
 import GenreContainer from '../genre/genre_container';
 import ReactPlayer from 'react-player';
+import YouTube from 'react-youtube';
+
+const opts = {
+  height: '100%',
+  width: '100%',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  playerVars: {
+    autohide: 1,
+    showinfo: 0,
+    autoplay: 1,
+    controls: 0,
+    loop: 1,
+    modestBranding: 1,
+    iv_load_policy: 3,
+    playlist:"6pxRHBw-k8M"
+  }
+};
+
+const browsePlayer = <YouTube videoId="6pxRHBw-k8M" alt="" opts={opts}/>;
 
 const Browse = ({ children }) => {
   return (
     <div>
       <DashboardContainer />
       <div className="video-wrapper">
-        <video autoPlay="true" loop="true">
-          <source src={window.assets.browseVideo} type="video/mp4"/>
-        </video>
+        { browsePlayer }
+        <div className="browse-video-motto">
+          <div className="browse-video-title">CineFlix Original: "4K"</div>
+          <div className="browse-video-description">A series of scenic videos with soothing background music</div>
+        </div>
+        <span className='black-square'></span>
       </div>
       <GenreContainer />
       { children }
@@ -21,4 +45,5 @@ const Browse = ({ children }) => {
 
 export default Browse;
 
-// <div className="browser-content"/>
+// <button className='play-now'>Play Now</button>
+// <button className='browse-my-list'>My List +</button>
